5be97e826737f901f4b4be97bf3c5b7c47e63496
Yizheng Huang <huangyz0918@gmail.com>
2020-11-05 10:53:30 +0800

    [feat] GPU support for Docker container manager

--- clipper_admin/clipper_admin/docker/docker_container_manager.py
+++ clipper_admin/clipper_admin/docker/docker_container_manager.py
@@ -30,6 +30,8 @@ logger = logging.getLogger(__name__)
 
 class DockerContainerManager(ContainerManager):
     def __init__(self,
+                 cuda_id=0,
+                 nvidia_runtime=False,
                  cluster_name="default-cluster",
                  docker_ip_address="localhost",
                  use_centralized_log=False,
@@ -76,6 +78,8 @@ class DockerContainerManager(ContainerManager):
             Any additional keyword arguments to pass to the call to
             :py:meth:`docker.client.containers.run`.
         """
+        self.cuda_id = cuda_id
+        self.nvidia_runtime = nvidia_runtime
         self.cluster_name = cluster_name
         self.cluster_identifier = cluster_name  # For logging purpose
         self.public_hostname = docker_ip_address
@@ -364,6 +368,7 @@ class DockerContainerManager(ContainerManager):
                 "No Clipper query frontend to attach model container to")
         query_frontend_hostname = containers[0].name
         env_vars = {
+            "CUDA_VISIBLE_DEVICES": self.cuda_id,
             "CLIPPER_MODEL_NAME": name,
             "CLIPPER_MODEL_VERSION": version,
             # NOTE: assumes this container being launched on same machine
@@ -380,13 +385,23 @@ class DockerContainerManager(ContainerManager):
         model_container_name = model_container_label + '-{}'.format(
             random.randint(0, 100000))
 
-        self.docker_client.containers.run(
-            image,
-            name=model_container_name,
-            environment=env_vars,
-            labels=labels,
-            log_config=self.log_config,
-            **self.extra_container_kwargs)
+        if self.nvidia_runtime:
+            self.docker_client.containers.run(
+                image,
+                runtime='nvidia',
+                name=model_container_name,
+                environment=env_vars,
+                labels=labels,
+                log_config=self.log_config,
+                **self.extra_container_kwargs)
+        else:
+            self.docker_client.containers.run(
+                image,
+                name=model_container_name,
+                environment=env_vars,
+                labels=labels,
+                log_config=self.log_config,
+                **self.extra_container_kwargs)
 
         # Metric Section
         add_to_metric_config(model_container_name, self.prom_config_path,
